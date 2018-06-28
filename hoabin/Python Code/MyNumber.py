d907e80a4a66c2d801f3004d03511af3b22959e4
karin224 <karin224@naver.com>
2018-06-28 23:12:19 +0900

    4장 숫자 맞추기 게임_level selection update

--- hoabin/Python Code/MyNumber.py
+++ hoabin/Python Code/MyNumber.py
@@ -2,8 +2,21 @@
 # 함수를 직접 만들어 써봅시다.
 import random
 
+# 단계를 선택합니다.
+level=input("숫자 맞추기 게임을 시작합니다. 단계를 골라주세요.\n 상:h 중:m 하:e")
+while level !="e" and level !="m" and level !="h":
+    level = input("죄송하지만 다시 입력해주세요. 상:h 중:m 하:e \n h/m/e:")
+
 # 숫자를 골라봅시다.
-computer_number = random.randint(1,100)
+if level == 'e':
+    computer_number = random.randint(1, 10)
+    highest_number = 10
+if level == 'm':
+   computer_number = random.randint(1,20)
+   highest_number = 20
+if level == 'h':
+    computer_number = random.randint(1, 100)
+    highest_number = 100
 
 # is_same() 함수를 만듭니다.
 def is_same(target, number):
@@ -16,7 +29,7 @@ def is_same(target, number):
     return result
 
 # 게임을 시작합니다.
-print("안녕,\n난 1부터 100 중 숫자 하나를 골랐어요.")
+print("안녕,\n난 1부터", highest_number, "중 숫자 하나를 골랐어요.")
 
 # 사용자가 추측한 숫자를 인수로 받아옵니다.
 guess = int(input("뭔지 쓰고 엔터키를 누르세요"))
